0de027b4bc406642f95d07e32e63d4264de0976b
andreikurnosov <nixdie@yandex.ru>
2021-01-21 00:57:13 +0700

    inital styling

---
+++ src/components/Song.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Song = () => {
+  return <div className="song-container">
+    <h1>Picture</h1>
+    <p>Song Name</p>
+    <p>Artist</p>
+  </div>
+}
+
+export default Song
