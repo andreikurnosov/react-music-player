c573ec599cc6595ed461388cc39d7e49d6ed29a5
andreikurnosov <nixdie@yandex.ru>
2021-01-25 16:42:14 +0700

    selecting songs

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-const LibrarySong = ({ song }) => {
+const LibrarySong = ({ song, songs, setCurrentSong, id }) => {
+  const songSelectHandler = () => {
+    setCurrentSong(song)
+  }
   return (
-    <div className="library-song">
+    <div onClick={songSelectHandler} className="library-song">
       <img src={song.cover} alt={song.name} />
-      <div className  ="song-description">
+      <div className="song-description">
         <h3>{song.name}</h3>
         <h4>{song.artist}</h4>
       </div>
