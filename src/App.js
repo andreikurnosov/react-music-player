d82be32d39a5eedb55fe7b8820763332ece0084b
andreikurnosov <nixdie@yandex.ru>
2021-02-01 10:46:43 +0700

    customizing input, changed title

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
   })
   const [libraryStatus, setLibraryStatus] = useState(false)
   const audioRef = useRef(null)
@@ -21,7 +22,15 @@ function App() {
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime
     const duration = e.target.duration
-    setSongInfo({ ...songInfo, currentTime: current, duration })
+    const roundedCurrent = Math.round(current)
+    const roundedDuration = Math.round(duration)
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100)
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration,
+      animationPercentage: animation,
+    })
   }
 
   return (
